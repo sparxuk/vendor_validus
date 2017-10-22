a1ce46569cdabe62764a65927edf537f6e94dc5a
Alex Cruz <du.alexcruz@gmail.com>
2017-10-21 22:21:52 -0400

    common: Add themes
    This makes use of AOSP's theme settings under Display. Adds two themes,
    a pixel one and a stock theme for users that want that AOSP look :-)
    
    Thanks to @Viperusgit for the heads on this and the prebuilt apk that was
    used to see how to go about recreating these type of themes
    
     *martinusbe
    changed to use BUILD_PREBUILT
    
    Change-Id: I6cf94aa261fe153b640dd2209aad700eab7835e6

--- config/common.mk
+++ config/common.mk
@@ -86,6 +86,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     AudioFX
 
+# Themes
+PRODUCT_PACKAGES += \
+    Pixel \
+    Stock
+
 # Extra Optional packages
 PRODUCT_PACKAGES += \
     Calculator \
