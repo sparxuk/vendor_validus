4b789785a87a3ed845c930607d0a39a16246cda0
martinusbe <martinusbe@gmail.com>
2018-01-16 17:49:16 +0100

    config: build aosp charger images
     * and disable unused cm charger completely
    
    Change-Id: I856d43f5cab5a2a965c839066e87f05f90e1a756

--- config/common.mk
+++ config/common.mk
@@ -110,10 +110,12 @@ PRODUCT_PACKAGES += \
     ntfsfix \
     ntfs-3g
 
+PRODUCT_PACKAGES += \
+    charger_res_images
+
 # Custom off-mode charger
 #ifneq ($(WITH_CM_CHARGER),false)
 #PRODUCT_PACKAGES += \
-#    charger_res_images \
 #    cm_charger_res_images \
 #    font_log.png \
 #    libhealthd.cm
