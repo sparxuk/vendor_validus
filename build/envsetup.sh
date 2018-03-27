0c576128ecf68f2fe50fd57b77bda45235c07a2d
Anay Wadhera <anay1018@gmail.com>
2018-03-27 15:22:41 -0700

    envsetup: include user variants
    Change-Id: I38b440c9074ea10fd258e0a271dc9f010e6021dc

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -6,7 +6,8 @@ function validus_device_combos()
 
     T="$(gettop)"
     list_file="${T}/vendor/validus/validus.devices"
-    variant="userdebug"
+    variant1="userdebug"
+    variant2="user"
 
     if [[ $1 ]]
     then
@@ -33,7 +34,8 @@ function validus_device_combos()
 
     while IFS= read -r device
     do
-        add_lunch_combo "validus_${device}-${variant}"
+        add_lunch_combo "validus_${device}-${variant1}"
+        add_lunch_combo "validus_${device}-${variant2}"
     done < "${list_file}"
 }
 
