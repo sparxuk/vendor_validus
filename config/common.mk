36f3f94631c6e15ae126aa6e065c0f02e24a2315
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-10-09 15:30:24 +0200

    config: Kill sysinit
    Change-Id: Ia8d46d794ff829f3fc39ff48d57fdb6f9c03262d

--- config/common.mk
+++ config/common.mk
@@ -68,12 +68,6 @@ PRODUCT_COPY_FILES += \
     vendor/validus/prebuilt/common/etc/permissions/privapp-permissions-gzr.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-gzr.xml \
     vendor/validus/prebuilt/common/etc/permissions/privapp-permissions-google.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-google.xml
 
-# Validus-specific startup services
-PRODUCT_COPY_FILES += \
-    vendor/validus/prebuilt/common/etc/init.d/00banner:$(TARGET_COPY_OUT_SYSTEM)/etc/init.d/00banner \
-    vendor/validus/prebuilt/common/etc/init.d/90userinit:$(TARGET_COPY_OUT_SYSTEM)/etc/init.d/90userinit \
-    vendor/validus/prebuilt/common/bin/sysinit:$(TARGET_COPY_OUT_SYSTEM)/bin/sysinit
-
 # Fonts
 PRODUCT_PACKAGES += \
     FontArbutusSourceOverlay \
