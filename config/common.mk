GZOSP/vendor_gzosp

gzosp: config: Don't block overlays from device
Change-Id: Ie565ce5b8593592f71b4bdee5e9614560deae0df

config/common.mk
@@ -145,7 +145,8 @@ PRODUCT_PACKAGES += \
     AndroidDarkThemeOverlay \
     SettingsDarkThemeOverlay
 
-PRODUCT_PACKAGE_OVERLAYS += vendor/gzosp/overlay/common
+# Common overlay
+DEVICE_PACKAGE_OVERLAYS += vendor/gzosp/overlay/common
 
 # Boot animation include
 ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))

sparxuk/vendor_validus

config/common.mk
@@ -145,7 +145,8 @@ PRODUCT_PACKAGES += \
     AndroidDarkThemeOverlay \
     SettingsDarkThemeOverlay
 
-PRODUCT_PACKAGE_OVERLAYS += vendor/validus/overlay/common
+# Common overlay
+DEVICE_PACKAGE_OVERLAYS += vendor/validus/overlay/common
 
 # Boot animation include
 ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))